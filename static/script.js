5dcd67bd995b075aa10bd52c8037496a205185d5
vaibhav gautam <loyalrespect2003@gmail.com>
2025-07-23 20:42:51 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-const backendURL = "http://127.0.0.1:8000";
+const backendURL = "https://historical-text-timeline.onrender.com";
 
 async function uploadPDF() {
     const fileInput = document.getElementById("pdf-file");
